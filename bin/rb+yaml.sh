fa96103d36a83c26bc11ac44bace028eba39bf30
Al Snow <jasnow@hotmail.com>
2026-02-06 12:18:08 -0500

    Did more copyright cleanup/update work

--- bin/rb+yaml.sh
+++ bin/rb+yaml.sh
@@ -24,17 +24,17 @@ fi
 
 if [ "X$1X" == "XmostX" ] ; then
 
-    echo
-    echo ">> rb - NOT COPYRIGHT"
+    echo ; echo ">> rb - NOT COPYRIGHT"
     fgrep -v -f /tmp/$$_C /tmp/$$_R |egrep -v "test/data|examples|Attic"
 
-    echo
-    echo ">> yaml - NOT COPYRIGHT"
+    echo ; echo ">> yaml - NOT COPYRIGHT"
     fgrep -v -f /tmp/$$_C /tmp/$$_Y |egrep -v "test/data|examples"
 
-    echo
-    echo ">> yaml - YES COPYRIGHT"
+    echo ; echo ">> yaml - YES COPYRIGHT"
     grep -i copyright $(cat /tmp/$$_Y) |egrep -v "test/data|examples"
+
+    echo ; echo ">> copyright IN test/data AND examples FILES"
+    grep -i copyright test/data/*/* examples/*/*
 fi
 
 rm /tmp/$$_Y /tmp/$$_R /tmp/$$_C
