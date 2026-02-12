d2eee748bf9da3a2896b6e49ad620ed053720861
Al Snow <jasnow@hotmail.com>
2026-02-12 11:41:39 -0500

    Did more work on my-tests.sh

--- bin/devbin/my-tests.sh
+++ bin/devbin/my-tests.sh
@@ -1,77 +1,110 @@
 #!/usr/bin/env bash
-TDU="test/data/users-guide"
+export TDU="test/data/users-guide"
+#cat <<WORKING >> /dev/null
 ######################################################################
 #YAML/YAML
 echo ; echo "2.1"
-./bin/kwalify -lf ${TDU}/schema01.yaml ${TDU}/document01a.yaml
+./bin/kwalify.rb -lf ${TDU}/schema01.yaml ${TDU}/document01a.yaml
 #
 echo ; echo "2.2"
-./bin/kwalify -lf ${TDU}/schema01.yaml ${TDU}/document01b.yaml
+./bin/kwalify.rb -lf ${TDU}/schema01.yaml ${TDU}/document01b.yaml
 #
 echo ; echo "3.1"
-./bin/kwalify -lf ${TDU}/schema02.yaml ${TDU}/document02a.yaml
+./bin/kwalify.rb -lf ${TDU}/schema02.yaml ${TDU}/document02a.yaml
 echo ; echo "3.2"
-./bin/kwalify -lf ${TDU}/schema02.yaml ${TDU}/document02b.yaml
+./bin/kwalify.rb -lf ${TDU}/schema02.yaml ${TDU}/document02b.yaml
 #
 echo ; echo "4.1"
-./bin/kwalify -lf ${TDU}/schema03.yaml ${TDU}/document03a.yaml
+./bin/kwalify.rb -lf ${TDU}/schema03.yaml ${TDU}/document03a.yaml
 echo ; echo "4.2"
-./bin/kwalify -lf ${TDU}/schema03.yaml ${TDU}/document03b.yaml
+./bin/kwalify.rb -lf ${TDU}/schema03.yaml ${TDU}/document03b.yaml
 #
 echo ; echo "5.1"
-./bin/kwalify -lf ${TDU}/schema04.yaml ${TDU}/document04a.yaml
+./bin/kwalify.rb -lf ${TDU}/schema04.yaml ${TDU}/document04a.yaml
 echo ; echo "5.2"
-./bin/kwalify -lf ${TDU}/schema04.yaml ${TDU}/document04b.yaml
+./bin/kwalify.rb -lf ${TDU}/schema04.yaml ${TDU}/document04b.yaml
 #
 echo ; echo "8.1"
-./bin/kwalify -lf ${TDU}/schema05.yaml ${TDU}/document05a.yaml
+./bin/kwalify.rb -lf ${TDU}/schema05.yaml ${TDU}/document05a.yaml
 echo ; echo "8.2"
-./bin/kwalify -lf ${TDU}/schema05.yaml ${TDU}/document05b.yaml
+./bin/kwalify.rb -lf ${TDU}/schema05.yaml ${TDU}/document05b.yaml
 #
 echo ; echo "9.1"
-./bin/kwalify -lf ${TDU}/schema06.yaml ${TDU}/document06a.yaml
+./bin/kwalify.rb -lf ${TDU}/schema06.yaml ${TDU}/document06a.yaml
 #
 echo ; echo "10.1"
-./bin/kwalify -lf ${TDU}/schema06.yaml ${TDU}/document06b.yaml
+./bin/kwalify.rb -lf ${TDU}/schema06.yaml ${TDU}/document06b.yaml
 ######################################################################
 #JSON/JSON
 echo ; echo "10.2"
-./bin/kwalify -lf ${TDU}/schema12.json ${TDU}/document12a.json
+./bin/kwalify.rb -lf ${TDU}/schema12.json ${TDU}/document12a.json
 #
 echo ; echo "11.1"
-./bin/kwalify -lf ${TDU}/schema12.json ${TDU}/document12b.json
+./bin/kwalify.rb -lf ${TDU}/schema12.json ${TDU}/document12b.json
 #
 ######################################################################
 #YAML/YAML
 echo ; echo "12.1"
-./bin/kwalify -lf ${TDU}/schema13.yaml ${TDU}/document13a.yaml
+./bin/kwalify.rb -lf ${TDU}/schema13.yaml ${TDU}/document13a.yaml
 echo ; echo "12.2"
-./bin/kwalify -lf ${TDU}/schema14.yaml ${TDU}/document14a.yaml
+./bin/kwalify.rb -lf ${TDU}/schema14.yaml ${TDU}/document14a.yaml
 #
 echo ; echo "13.1"
-./bin/kwalify -lf ${TDU}/schema14.yaml ${TDU}/document14b.yaml
+./bin/kwalify.rb -lf ${TDU}/schema14.yaml ${TDU}/document14b.yaml
 #
 echo ; echo "14.1"
-./bin/kwalify -lf ${TDU}/schema15.yaml ${TDU}/document15a.yaml
+./bin/kwalify.rb -lf ${TDU}/schema15.yaml ${TDU}/document15a.yaml
 echo ; echo "14.2"
-./bin/kwalify -lf ${TDU}/schema15.yaml ${TDU}/document15b.yaml
-#
-######################################################################
-#RUBY
-PATH="./bin:${PATH}"
-echo ; echo "14.3"
-echo "BROKEN: Can't find answers-schema.yaml"
-#ruby -I lib:${TDU} ${TDU}/validate08.rb
+./bin/kwalify.rb -lf ${TDU}/schema15.yaml ${TDU}/document15b.yaml
+
+echo "PUT 14.3 HERE"  
 
 echo ; echo "15.1 (schema.yaml unknown so changed to kwalify.schema.yaml//SKIPPED"
 echo ; echo "15.2 (schema.yaml unknown so changed to kwalify.schema.yaml//SKIPPED"
 #
 echo ; echo "16.1: NOT FULL EXAMPLE/SKIPPED"
-#./bin/kwalify -m ${TDU}/schema1.yaml ${TDU}/schema2.yaml"
+#./bin/kwalify.rb -m ${TDU}/schema1.yaml ${TDU}/schema2.yaml"
 #
+echo ; echo "27.1 (WORKED 1st TIME)"
+./bin/kwalify.rb -ha ./lib/kwalify/templates/genclass-ruby
+echo ; echo "27.2"
+./bin/kwalify.rb -ha ./lib/kwalify/templates/genclass-ruby --module=My --hashlike
+#
+echo ; echo "27.3 (WORKED 1st TIME)"
+./bin/kwalify.rb -a ./lib/kwalify/templates/genclass-java \
+    -tf ${TDU}/address_book.schema.yaml
+rm -f AddressBook.java  Group.java  Person.java
+#
+echo ; echo "30.1.1"
+echo "DEFERRED: No jar file - javac -nowarn -classpath '.:kwalify.jar' *.java"
+# -Xlint:unchecked => "warning: [unchecked] unchecked call to
+#    set(int,E) as a member of the raw type List
+#List<Group> seq = new ArrayList<>();
+#List<Person> seq = new ArrayList<>();
+#
+echo ; echo "30.1.2"
+echo "DEFERRED: No jar file - javac -classpath '.:kwalify.jar' ${TDU}/ExampleAddressBook"
+#
+echo ; echo "31.1 (WORKED 1st TIME)"
+./bin/kwalify.rb -ha ./lib/kwalify/templates/genclass-java
+#
+echo ; echo "31.2"
+echo 'DEFERRED: Getting: "schema filename is not specified."'
+./bin/kwalify.rb -a ./lib/kwalify/templates/genclass-java \
+  --package=com.example.my --implements=Serializable --basedir=src
+
+exit
+#
+######################################################################
+#RUBY
+#export PATH=../bin:${PATH}
+#export RUBYLIB=${TDU}:${RUBYLIB}
+
+echo ; echo "14.3"
+echo "BROKEN: Can't find answers-schema.yaml - ruby -I ../bin:${TDU} ${TDU}/validate08.rb"
+
 echo ; echo "17.1"
-echo "BROKEN: Can't find ${TDU}/answers-schema.yaml"
-#ruby -I lib:${TDU} ${TDU}/answers-validator.rb ${TDU}/document07a.yaml
+echo "BROKEN: Can't find ${TDU}/answers-schema.yaml - ruby -I lib:${TDU} ${TDU}/answers-validator.rb ${TDU}/document07a.yaml"
 #
 echo ; echo "18.1"
 echo "BROKEN: Can't find kwalify - ruby ${TDU}/howto3.rb"
@@ -94,36 +127,5 @@ echo "BROKEN: Can't find kwalify/util/ordered-hash"
 #    -tf ${TDU}/address_book.schema.yaml > address_book.rb
 #
 echo ; echo "26.1"
-echo "BROKEN: Can't find address_book.yaml"
-#ruby -I lib:${TDU} ${TDU}/example_address_book.rb
+echo "BROKEN: Can't find address_book.yaml - ruby -I lib:${TDU} ${TDU}/example_address_book.rb"
 #
-echo ; echo "27.1 (WORKED 1st TIME)"
-./bin/kwalify -ha ./lib/kwalify/templates/genclass-ruby
-
-echo ; echo "27.2"
-./bin/kwalify -ha ./lib/kwalify/templates/genclass-ruby --module=My --hashlike
-
-#
-echo ; echo "27.3 (WORKED 1st TIME)"
-./bin/kwalify -a ./lib/kwalify/templates/genclass-java \
-    -tf ${TDU}/address_book.schema.yaml
-
-echo ; echo "30.1.1"
-# -Xlint:unchecked => "warning: [unchecked] unchecked call to
-#    set(int,E) as a member of the raw type List
-#List<Group> seq = new ArrayList<>();
-#List<Person> seq = new ArrayList<>();
-javac -nowarn -classpath '.:kwalify.jar' *.java
-
-echo ; echo "30.1.2"
-echo "SKIPPED: No jar file - javac -classpath '.:kwalify.jar' ${TDU}/ExampleAddressBook"
-#
-echo ; echo "31.1 (WORKED 1st TIME)"
-./bin/kwalify -ha ./lib/kwalify/templates/genclass-java
-#
-echo ; echo "31.2"
-echo 'BROKEN: Getting: "schema filename is not specified."'
-./bin/kwalify -a ./lib/kwalify/templates/genclass-java \
-    --package=com.example.my --implements=Serializable --basedir=src
-#
-#EOF
