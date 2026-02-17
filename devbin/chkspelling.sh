fe38933573e0c5c38f756d8b1795d59856b6c0aa
Al Snow <43523+jasnow@users.noreply.github.com>
2026-02-17 13:54:17 -0500

    More chkspelling.sh work

--- devbin/chkspelling.sh
+++ devbin/chkspelling.sh
@@ -1,25 +1,25 @@
 #!/usr/bin/env bash
 
-#if [ "X$1X" == "XX" ] ; then
-#    echo "Usage: $0 file"
-#    exit
-#fi
-
+# CODE WORDS
 cat $(find . -type f |egrep -v ".git/|coverage/") \
 | tr -cs "[:alpha:]" "[\n*]" \
-| tr "[A-Z]" "{a-z]" |sort -u > /tmp/$$_u
+| tr "[A-Z]" "{a-z]" |sort -u > /tmp/$$_o_u
+
+# Create one file.
+# MY INITIAL SW WORDS: devbin/ruby_rails_wordlist.txt
+# DOC WORDS:           devbin/ruby_docs_wordlist.txt
+sort -u \
+  devbin/ruby_rails_wordlist.txt \
+  devbin/ruby_docs_wordlist.txt > /tmp/$$_o_b
 
 #INTERACTIVE: aspell list /tmp/$$_u
-
-#(spell -d devbin/ruby_rails_wordlist.txt /tmp/$$_u
 (
-spell -d devbin/ruby_rails_wordlist.txt /tmp/$$_u
+spell /tmp/$$_o_u
 echo "---"
-hunspell -l /tmp/$$_u
+hunspell -l /tmp/$$_o_u
 ) | sort -u
 # | pr -3 |uniq
 
-rm /tmp/$$_u
-
+rm /tmp/$$_o_[bu]
 
 # YES TO: egrep -v "html$|yaml$|json$|rb$|result|java"
